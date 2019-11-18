7aa66e78525fb581969477c7082956fa4fd4e00b
Jorge Fernández <44475543+ixa10@users.noreply.github.com>
2019-11-18 23:15:20 +0100

    CreateResources

--- Assets/Scripts/CreateGameGrid.cs
+++ Assets/Scripts/CreateGameGrid.cs
@@ -7,13 +7,29 @@ public class CreateGameGrid : MonoBehaviour
     
     public GameObject casillaInstanced;
     private Vector3 pos;
+
+    private GameObject[] casillasArray;
+    private int index;
+    private int wood, stone, gold, food;
     
     // Start is called before the first frame update
     void Start()
     {
         Vector3 pos = new Vector3 (-7.5f, -1f, 7.5f);
         
-        createGrid();
+        casillasArray = new GameObject[256];
+        
+        CreateGrid();
+
+        index = 0;
+        
+        wood = 32;
+        stone = 32;
+        gold = 32;
+        food = 32;
+
+        //CreateResources();
+        
     }
 
     // Update is called once per frame
@@ -22,7 +38,7 @@ public class CreateGameGrid : MonoBehaviour
         
     }
 
-    void createGrid(){
+    void CreateGrid(){
         for (int i = 0; i < 16; i++){
             for (int j = 0; j < 16; j++){
                 if(i == 0 && j == 0){
@@ -30,11 +46,27 @@ public class CreateGameGrid : MonoBehaviour
                     pos.z = 7.5f;
                     pos.y = -0.5f;
                 }
-                Instantiate(casillaInstanced, pos, Quaternion.identity);
+                GameObject casilla = Instantiate(casillaInstanced, pos, Quaternion.identity);
+                casilla.transform.GetChild(0).gameObject.SetActive(false);
+                casilla.transform.GetChild(1).gameObject.SetActive(false);
+                casilla.transform.GetChild(2).gameObject.SetActive(false);
+                casilla.transform.GetChild(3).gameObject.SetActive(false);
+                casillasArray[index] = casilla;
+                index++;
                 pos.x += 1f;
             }
             pos.x = -7.5f;
             pos.z-= 1f;
+        }
+
+
+    }
+
+    void CreateResources(){
+        for (int i = 0; i < 32; i++){
+            int fila = Random.Range(0,16);
+            int col = Random.Range(0,8);
+            int colReflex = 15-col;
         }
 
 

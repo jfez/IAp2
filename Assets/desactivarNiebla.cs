16a5bd47ade05f307424f6245e9df191097e35b7
Sergio Valdelvira Pérez <al315632@uji.es>
2019-12-10 18:20:28 +0100

    Niebla de Guerra

---
+++ Assets/desactivarNiebla.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class desactivarNiebla : MonoBehaviour
+{ 
+    public Collider collider;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        collider.GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+            Debug.Log("entra");
+        }
+    }
+}
